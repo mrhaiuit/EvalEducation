b89814a3828ed59847c3ef0d6c0bd70ddc50e22f
davidtrinh173 <davidtrinh173@gmail.com>
2020-02-13 14:42:28 +0700

    Delete

---
+++ BackEnd/EvalEduation/Components/Commons/EVE.ApiModels/Catalog/Request/EvalCriteria/GetGuideOfCriteriaReq.cs
@@ -0,0 +1,27 @@
+﻿using EVE.Commons;
+using EVE.WebApi.Shared;
+using FluentValidation;
+using FluentValidation.Attributes;
+
+namespace EVE.ApiModels.Catalog
+{
+    [Validator(typeof(GetGuideOfCriteriaValidator))]
+    public class GetGuideOfCriteriaReq 
+    {
+        public string EvalResultCode { get; set; }
+        public int EvalCriteriaId { get; set; }
+    }
+
+    public class GetGuideOfCriteriaValidator : AbstractValidator<GetGuideOfCriteriaReq>
+    {
+        public GetGuideOfCriteriaValidator()
+        {
+            RuleFor(c => c.EvalCriteriaId)
+                    .NotNull()
+                    .NotEmpty();
+            RuleFor(c => c.EvalResultCode)
+                    .NotNull()
+                    .NotEmpty();
+        }
+    }
+}
