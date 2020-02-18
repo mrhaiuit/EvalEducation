01b9f9e6b796dad8f337c362ac75883e30f1e697
mrhaiuit <davidtrinh173@gmail.com>
2020-02-18 07:26:57 +0700

    chỉnh sua projet

---
+++ BackEnd/EvalEduation/Components/Commons/EVE.ApiModels/Authentication/Response/Account/LogonResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EVE.ApiModels.Authentication.Response
+{
+    public class LogonResponse
+    {
+        public LogonResponse()
+        {
+            
+        }
+
+        public string USER_ID { get; set; }
+
+        public string IP_ADDRESS { get; set; }
+
+        public string USER_NAME { get; set; }
+
+        public DateTime LOGON_DATE { get; set; }
+
+        public DateTime UPD_TS { get; set; }
+    }
+}
